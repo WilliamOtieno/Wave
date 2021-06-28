7698555ac804a74a2a7c56fe8af7d29ce07fd7a7
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-06-28 13:19:04 +0300

    refactor

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -29,7 +29,7 @@ const RegisterScreen = ({ navigation }) => {
     return (
         <KeyboardAvoidingView behavior='padding' style={styles.container} >
             <StatusBar style='light' />
-            <Text h3 style={{marginBottom: 50}} >Create a Wave Account</Text>
+            <Text h3 style={{marginBottom: 50}} >Create a new Sigma Account</Text>
             <View style={styles.inputContainer}>
                 <Input placeholder='Full Name' autofocus type='text' value={name} onChangeText={(text) => setName(text)}/>
                 <Input placeholder='Email Address' type='email' value={email} onChangeText={(text) => setEmail(text)}/>
