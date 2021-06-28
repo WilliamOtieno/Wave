557d24c53fdd23110311ce93c5f449edae0cd69f
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-06-28 04:08:45 +0300

    refactor

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -18,10 +18,11 @@ const RegisterScreen = ({ navigation }) => {
     const register = () => {
         auth.createUserWithEmailAndPassword(email, password)
         .then(authUser => {
-            authUser.user.update({
+            authUser.user.updateProfile({
                 displayName: name,
                 photoURL: imageUrl || 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
             })
+            console.log(authUser);
         }).catch(error => alert(error.message))
     }
 
@@ -30,10 +31,10 @@ const RegisterScreen = ({ navigation }) => {
             <StatusBar style='light' />
             <Text h3 style={{marginBottom: 50}} >Create a Wave Account</Text>
             <View style={styles.inputContainer}>
-                <Input placeholder='Full Name' autofocus type='text' value={name} onChange={(text) => setName(text)}/>
-                <Input placeholder='Email Address' type='email' value={email} onChange={(text) => setEmail(text)}/>
-                <Input placeholder='Password' secureTextEntry type='password' value={password} onChange={(text) => setPassword(text)}/>
-                <Input placeholder='Profile Picture URL (Optional)' type='text' value={imageUrl} onChange={(text) => setImageUrl(text)} onSubmitEditing={register} />
+                <Input placeholder='Full Name' autofocus type='text' value={name} onChangeText={(text) => setName(text)}/>
+                <Input placeholder='Email Address' type='email' value={email} onChangeText={(text) => setEmail(text)}/>
+                <Input placeholder='Password' secureTextEntry type='password' value={password} onChangeText={(text) => setPassword(text)}/>
+                <Input placeholder='Profile Picture URL (Optional)' type='text' value={imageUrl} onChangeText={(text) => setImageUrl(text)} onSubmitEditing={register} />
             </View>
             
             <Button containerStyle={styles.button} raised={true} title='Register' onPress={register}/>
