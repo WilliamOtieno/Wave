9f43a39bf577250bfcce70a43d34470d81b7cfd5
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-06-28 13:19:30 +0300

    Home Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,48 @@
-import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
+import CustomListItem from '../components/CustomListItem'
+import { auth, db } from '../firebase'
+import { Avatar } from 'react-native-elements'
+import { AntDesign, SimpleLineIcons } from '@expo/vector-icons'
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+
+    const signUserOut = () => {
+        auth.signOut().then(() => {
+            navigation.replace('Login')
+        })
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: "Sigma",
+            headerStyle: { backgroundColor:'#fff' },
+            headerTitleStyle: { color:'black' },
+            headerTintColor: 'black',
+            headerLeft: () => (
+                <View style={{marginLeft: 20}} >
+                    <TouchableOpacity onPress={signUserOut} activeOpacity={0.5} >
+                        <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+                    </TouchableOpacity>
+                </View>
+            ),
+            headerRight: () => (
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', width: 80, marginRight: 20}}>
+                    <TouchableOpacity activeOpacity={0.5}>
+                        <AntDesign name='camerao' size={24} color='black' />
+                    </TouchableOpacity>
+                    <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.navigate('AddChat')} >
+                        <SimpleLineIcons name='pencil' size={24} color='black'/>
+                    </TouchableOpacity>
+                </View>
+            )
+        })
+    }, [navigation])
+
     return (
         <SafeAreaView>
             <ScrollView>
-
+                <CustomListItem />
             </ScrollView>
         </SafeAreaView>
     )
