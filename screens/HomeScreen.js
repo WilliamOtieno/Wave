3c9395d05a8bf25055dd53c80f59147a5b463dfd
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-06-28 04:08:28 +0300

    homescreen view

---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <SafeAreaView>
+            <ScrollView>
+
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
