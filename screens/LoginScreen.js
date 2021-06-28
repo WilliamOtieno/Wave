cbd8f7d949dba464974bc420f6eef65c885aafa4
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-06-28 03:12:59 +0300

    login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native'
 import { Button, Input, Image } from 'react-native-elements'
 import { StatusBar } from 'expo-status-bar'
+import { auth } from '../firebase';
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((authUser) => {
+            if (authUser) {
+                navigation.replace('Home')
+            }
+        });
+        return unsubscribe;
+    }, []);
 
     const signIn = () => {
 
@@ -17,14 +27,14 @@ const LoginScreen = () => {
             <StatusBar style="light" />
             <Image 
                 source={{uri: 'https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png'}} 
-                style={{width: 200, height: 200}}
+                style={{width: 100, height: 100}}
             />
             <View style={styles.inputContainer}>
                 <Input placeholder='Email' autofocus type="email" value={email} onChangeText={(text) => setEmail(text)} />
                 <Input placeholder='Password' secureTextEntry type="password" value={password} onChangeText={(text) => setPassword(text)}/>
             </View>
             <Button containerStyle={styles.button} onPress={signIn} title='Login'  />
-            <Button containerStyle={styles.button} type='outline' title='Register'  />
+            <Button onPress={() => navigation.navigate('Register')} containerStyle={styles.button} type='outline' title='Register'  />
         </KeyboardAvoidingView>
     )
 }
